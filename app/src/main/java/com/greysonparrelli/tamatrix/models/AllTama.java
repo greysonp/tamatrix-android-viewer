88743ba008889a68c6349ebf1e5a0ac910cfe3d2
Greyson Parrelli <greyson.parrelli@gmail.com>
2016-07-12 09:45:32 -0700

    Cleaned up padding.

--- app/src/main/java/com/greysonparrelli/tamatrix/models/AllTama.java
+++ app/src/main/java/com/greysonparrelli/tamatrix/models/AllTama.java
@@ -6,9 +6,4 @@ public class AllTama {
     public int notama;
     public long lastseq;
     public List<Tama> tama;
-
-    @Override
-    public String toString() {
-        return tama.get(0).toString();
-    }
 }
